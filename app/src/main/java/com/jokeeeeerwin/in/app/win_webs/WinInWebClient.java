e3f6bd0150b69be3e82eebd581697f89394ed7d7
Korniienko Denys <71882300+Reksagon@users.noreply.github.com>
2021-08-06 17:20:11 +0300

    44

--- app/src/main/java/com/jokeeeeerwin/in/app/win_webs/WinInWebClient.java
+++ app/src/main/java/com/jokeeeeerwin/in/app/win_webs/WinInWebClient.java
@@ -9,8 +9,10 @@ public class WinInWebClient extends WebViewClient {
     @Override
     public boolean shouldOverrideUrlLoading(WebView view, String url) {
         if(!url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cudGlrdG9rLmNvbS8=", Base64.DEFAULT)))
+                ||! url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cubGlua2VkaW4uY29tLw==", Base64.DEFAULT)))
                 ||!url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cuaW5zdGFncmFtLmNvbS8=", Base64.DEFAULT)))
-                ||! url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cubGlua2VkaW4uY29tLw==", Base64.DEFAULT))))
+                ||!url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cubGlua2VkaW4uY29tLw==", Base64.DEFAULT)))
+                ||!url.startsWith(new String(Base64.decode("aHR0cHM6Ly93d3cuZmFjZWJvb2suY29tLw==", Base64.DEFAULT))))
             view.loadUrl(url);
 
         return true;
