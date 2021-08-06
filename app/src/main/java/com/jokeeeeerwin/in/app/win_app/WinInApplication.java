64417444ee4d81305629343e57e61d2671b4a595
Korniienko Denys <71882300+Reksagon@users.noreply.github.com>
2021-08-06 14:50:03 +0300

    22

--- app/src/main/java/com/jokeeeeerwin/in/app/win_app/WinInApplication.java
+++ app/src/main/java/com/jokeeeeerwin/in/app/win_app/WinInApplication.java
@@ -2,5 +2,19 @@ package com.jokeeeeerwin.in.app.win_app;
 
 import android.app.Application;
 
+import com.onesignal.OneSignal;
+import com.yandex.metrica.YandexMetrica;
+import com.yandex.metrica.YandexMetricaConfig;
+
 public class WinInApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        YandexMetricaConfig yaconfig = YandexMetricaConfig.newConfigBuilder("3a29f386-9e7e-43fc-95d3-0b56166e7b12").build();
+        OneSignal.initWithContext(getApplicationContext());
+        YandexMetrica.activate(getApplicationContext(), yaconfig);
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+        YandexMetrica.enableActivityAutoTracking(this);
+        OneSignal.setAppId("b1496846-555c-44bb-a36a-20902d432d6d");
+    }
 }
